da22a1941fb99efb015e52014642b774d8b81e69
ljaeh <wogurdlek33@gmail.com>
2024-02-29 17:21:09 +0900

    marketstart

---
+++ backend/src/main/java/org/ljaeh/boook/backend/ItemController.java
@@ -0,0 +1,22 @@
+package org.ljaeh.boook.backend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemController {
+
+	@GetMapping("/api/items")
+	public List<String> getItems(){
+		List<String> items = new ArrayList<>();
+		
+		items.add("alpha");
+		items.add("beta");
+		items.add("gamma");
+		
+		return items;
+	}
+}
---
+++ backend/src/test/java/org/ljaeh/boook/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ljaeh.boook.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
